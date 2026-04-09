4c433deb1044d7ed2a400bee6f0ac81d1b0989da
Beeltraan25 <beltran.almendros@edu.uah.es>
2026-04-09 19:33:33 +0200

    Delete _/apps directory

--- _/apps/web/src/constants/colors.js
+++
@@ -1,7 +0,0 @@
-export const COLORS = {
-  primary: "#2D5A27",
-  secondary: "#A68A64",
-  text: "#333333",
-  light: "#F8F9F8",
-  white: "#FFFFFF",
-};
--- _/apps/web/src/data/navigation.js
+++
@@ -1,7 +0,0 @@
-export const navLinks = [
-  { name: "Inicio", href: "#inicio" },
-  { name: "El Campo", href: "#campo" },
-  { name: "Servicios", href: "#servicios" },
-  { name: "Tarifas", href: "#tarifas" },
-  { name: "Contacto", href: "#contacto" },
-];
